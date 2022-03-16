272813d0f08269e766c6454f0f6799075ff07fdf
Tejaswin Parthasarathy <tp5@illinois.edu>
2022-03-16 02:02:46 -0500

    refactor : compiler detection step for reuse

--- detail/build_blaze.sh
+++ detail/build_blaze.sh
@@ -5,25 +5,8 @@ BLAZE_BUILD_DIR="build"
 BLAZE_INSTALL_PREFIX=${1:-"${HOME}/Desktop/third_party_installed"}
 mkdir -p "${BLAZE_INSTALL_PREFIX}"
 
-# check gcc version starting from 9 on to 4
-version_array=($(seq 11 -1 4))
-CXX_ver_arr=("${version_array[@]/#/g++-}")
-CC_ver_arr=("${version_array[@]/#/g++-}")
-# Try and detect GNU g++ from the shell, if not use default CC
-for cxx_ver in "${CXX_ver_arr[@]}"; do
-	if command -v "${cxx_ver}" >/dev/null 2>&1 && "${cxx_ver}" --version | grep -q '[Gg][Cc][Cc]'; then
-		CXX="${cxx_ver}"
-		break
-	fi
-done
-# Check if not set, else set it
-if [ -z "${CXX}" ] && g++ --version; then
-	CXX="g++"
-fi
-if [ -z "${CXX}" ]; then
-	# We have no hope, fall back on XZ's g++
-	CXX="/usr/local/bin/g++"
-fi
+_CXX_="garbage"
+source "./detect_compiler.sh" # populates _CXX_
 
 # >3.14
 # -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
---
+++ detail/detect_compiler.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+function detect_compiler() {
+	# check gcc version starting from 9 on to 4
+	local version_array=($(seq 11 -1 4))
+	local CXX_ver_arr=("${version_array[@]/#/g++-}")
+	local CC_ver_arr=("${version_array[@]/#/g++-}")
+	# Try and detect GNU g++ from the shell, if not use default CC
+	for cxx_ver in "${CXX_ver_arr[@]}"; do
+		if command -v "${cxx_ver}" >/dev/null 2>&1 && "${cxx_ver}" --version | grep -q '[Gg][Cc][Cc]'; then
+			_CXX_="${cxx_ver}"
+			break
+		fi
+	done
+	# Check if not set, else set it
+	if [ -z "${_CXX_}" ] && g++ --version; then
+		_CXX_="g++"
+	fi
+}
