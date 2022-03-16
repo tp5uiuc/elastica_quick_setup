b5c9a786467560c1319c03079bb2b997d339e38a
Tejaswin Parthasarathy <tp5@illinois.edu>
2022-03-16 02:36:35 -0500

    refactor : revert to old style compiler-detection

--- detail/build_blaze.sh
+++ detail/build_blaze.sh
@@ -5,8 +5,23 @@ BLAZE_BUILD_DIR="build"
 BLAZE_INSTALL_PREFIX=${1:-"${HOME}/Desktop/third_party_installed"}
 mkdir -p "${BLAZE_INSTALL_PREFIX}"
 
-_CXX_="garbage"
-source "./elastica_detect_compiler.sh" # populates _CXX_
+function elastica_detect_compiler() {
+	# check gcc version starting from 9 on to 4
+	local version_array=($(seq 11 -1 4))
+	local CXX_ver_arr=("${version_array[@]/#/g++-}")
+	local CC_ver_arr=("${version_array[@]/#/g++-}")
+	# Try and detect GNU g++ from the shell, if not use default CC
+	for cxx_ver in "${CXX_ver_arr[@]}"; do
+		if command -v "${cxx_ver}" >/dev/null 2>&1 && "${cxx_ver}" --version | grep -q '[Gg][Cc][Cc]'; then
+			_CXX_="${cxx_ver}"
+			break
+		fi
+	done
+	# Check if not set, else set it
+	if [ -z "${_CXX_}" ] && g++ --version; then
+		_CXX_="g++"
+	fi
+}
 elastica_detect_compiler
 
 # >3.14
